81b6ded70feb0dd10da60feb0c017ac393c2cbdb
Ekaterina Terenicheva <Eventcpa@gmail.com>
2020-12-09 19:42:18 +0600

    внесены правки и изменения в код

---
+++ Assets/Script/SliderOfHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderOfHealth : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private Player _player;
+
+    private void OnEnable()
+    {
+        _player.HealthChanged += OnChangeHealth;
+    }
+
+    private void OnDisable()
+    {
+        _player.HealthChanged -= OnChangeHealth;
+    }
+
+    private void OnChangeHealth(float value)
+    {
+        _slider.value = value;
+    }
+}
